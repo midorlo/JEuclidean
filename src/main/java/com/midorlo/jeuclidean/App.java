f7d6b6723d2281799be5c51454fc904b4b2dae53
midorlo <dorloechter@gmail.com>
2022-03-24 00:25:57 +0100

    init

---
+++ src/main/java/com/midorlo/jeuclidean/App.java
@@ -0,0 +1,15 @@
+package com.midorlo.jeuclidean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+
+        log.info("Hello world!");
+        log.warn("Hello world!");
+        log.error("Hello world!");
+        log.debug("Hello world!");
+    }
+}
